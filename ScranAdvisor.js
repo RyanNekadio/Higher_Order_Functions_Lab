5b160bf09de4523561af2c787f2019e2a8b50940
RyanNekadio <ryanekadio12@hotmail.co.uk>
2023-11-22 15:58:21 +0000

    Uploading completed MVP tasks for lab

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -3,5 +3,26 @@ const ScranAdvisor = function(restaurants){
 }
 
 // Add methods to prototype
+ScranAdvisor.prototype.countRestaurants = function() {
+    let count = 0;
+
+    for (const element of this.restaurants){
+        count += 1;
+    }
+
+    return count;
+}
+
+ScranAdvisor.prototype.findRestaurant = function(restaurantName){
+    return this.restaurants.find((restaurant) => restaurant.name === restaurantName);
+};
+
+ScranAdvisor.prototype.allRestaurantNames = function(){
+    return this.restaurants.map(restaurant => restaurant.name);
+}
+
+ScranAdvisor.prototype.findRestaurantByCity = function(city){
+    return this.restaurants.filter(restaurant => restaurant.location.town === city);
+}
 
 module.exports = ScranAdvisor;
